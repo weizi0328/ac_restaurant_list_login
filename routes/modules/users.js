eef0d61e25ef56ecdef45ade65b12cb542225896
weizi0328 <ankewang26@gmail.com>
2022-08-28 23:22:29 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -40,6 +40,11 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 
 
 
